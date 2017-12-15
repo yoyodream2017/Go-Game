5f1148f675fb03bbeef0efac12a032524c8ca0e3
Leon Yu <lyu@Leon-Yus-MBP.local>
2017-12-15 09:21:53 +0800

    [add-unit-test] basic building on jest

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # My-React
-This code is mainly copied from react official website with some refactor 
+This code is mainly copied from react official website with some refactor.
+The React provides a simple tic tac toe and we release the code in release one.
+We did the refactor and provided a gobang game by extending the exist components.
+
+# Basic Operations
+npm (run) start: open the app locally with eslint
+npm (run) test: run test on src
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,147 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../App'
+/* 
+  This test includes some basic test to learn jest.
+*/
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+import App from '../App'
+const fetchData = jest.fn()
+
+/*
+  asynchronous test
+*/
+
+// this callback is asynchronous pattern, and will not be detected since jest will finish once it reach the end.
+test('the data is 122', () => {
+  function callback(data) {
+    setTimeout(function(){
+      expect(data).toBe('122');
+    },1000)
+  }
+  
+  fetchData(callback('1222'));
+});
+
+// This coding type of mock setTimeout will leads to test time out, just skip
+test.skip('the data is 122', done => {
+  function callback(data) {
+    setTimeout(function(){
+      expect(data).toBe('122');
+      done()
+    },1000)
+  }
+  
+  fetchData(callback('1222'));
+});
+
+// one choice for asynchronous
+test('mock setTimeout test', async () => {
+  let data = 0
+  await new Promise(resolve => {
+    setTimeout(() => {
+      data = 2
+      resolve();
+    }, 1000);
+  });
+
+  expect(data).toBe(2);
+});
+
+/*
+  setup 
+  beforeEach
+  afterEach
+*/
+// const isCity = {
+//   'Vienna': false,
+//   'San Juan': false
+// }
+// function initializeCityDatabase () {
+//   isCity['Vienna'] = !isCity['Vienna']
+//   isCity['San Juan'] = !isCity['San Juan']
+// }
+
+// function clearCityDatabase () {
+//   isCity['Vienna'] = false
+//   isCity['San Juan'] = false
+// }
+
+// beforeEach(() => {
+//   initializeCityDatabase();
+// });
+
+// afterEach(() => {
+//   clearCityDatabase();
+// });
+
+// test('city database has Vienna', () => {
+//   expect(isCity['Vienna']).toBeTruthy();
+// });
+
+// test('city database has San Juan', () => {
+//   expect(isCity['San Juan']).toBeTruthy();
+// });
+
+/*
+  teardown
+*/
+const isCity = {
+  'Vienna': false,
+  'San Juan': true
+}
+function initializeCityDatabase () {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      isCity['Vienna'] = true
+      resolve();
+    }, 1000);
+  });
+}
+
+function clearCityDatabase () {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      isCity['Vienna'] = false
+      isCity['San Juan'] = false
+      resolve();
+    }, 1000);
+  });
+}
+
+beforeAll(() => {
+  return initializeCityDatabase();
+});
+
+afterAll(() => {
+  return clearCityDatabase();
+});
+
+/* 
+  run the following would lead error
+*/
+// beforeEach(() => {
+//   return initializeCityDatabase();
+// });
+
+// afterEach(() => {
+//   return clearCityDatabase();
+// });
+
+test('city database has Vienna', () => {
+  expect(isCity['Vienna']).toBeTruthy();
+});
+
+test('city database has San Juan', () => {
+  expect(isCity['San Juan']).toBeTruthy();
+});
+
+//describe('Scoped / Nested block', () => {
+//   beforeAll(() => console.log('2 - beforeAll'));
+//   afterAll(() => console.log('2 - afterAll'));
+//   beforeEach(() => console.log('2 - beforeEach'));
+//   afterEach(() => console.log('2 - afterEach'));
+//   test('', () => console.log('2 - test'));
+// });
+
+// add only would skip all the others on the page
+test.only('this will be the only test that runs', () => {
+  expect(true).toBe(true);
+});
